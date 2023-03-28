f8cffe2070ff2709b708a4b60f44d5e0d05caa8c
Stanley Magno Alves Brandao <brandaostanley0@gmail.com>
2023-03-28 19:11:13 -0300

    segunda resolução

--- JavaScript-PrimeirosPassos/secao-3-JavaScript-PrimeirosPassos/dia-1-PrimeirosPassos/Script.js
+++ JavaScript-PrimeirosPassos/secao-3-JavaScript-PrimeirosPassos/dia-1-PrimeirosPassos/Script.js
@@ -1,8 +1,8 @@
 let a = 3;
 let b = 8;
 
-console.log('Soma: ' + (a + b));
-console.log('Subtração: ' + (a - b));
-console.log('Multiplicação: ' + (a * b));
-console.log('Divisão: ' + (a / b));
-console.log('Módulo: ' + (a % b));
+if (a > b) {
+  console.log("Parabéns 'a' é maior que 'b'!");
+} else {
+  console.log("Que pena 'a' é menor que 'b'.");
+};
